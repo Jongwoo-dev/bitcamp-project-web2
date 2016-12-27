dc63a7b8a0eeafe6a55d120b55c74e1e8895fa0f
Jongwoo.dev <Jongwoo.dev@gmail.com>
2016-12-27 15:18:42 +0900

    v1.3 - 로그인 할 떄 사용자의 유형을 지정하고, 로그인 사용자의 사진 출력

--- src/main/java/bitcamp/java89/ems2/dao/MemberDao.java
+++ src/main/java/bitcamp/java89/ems2/dao/MemberDao.java
@@ -6,9 +6,9 @@ import bitcamp.java89.ems2.util.DataSource;
 public interface MemberDao {
   public void setDataSource(DataSource ds);
   public boolean exist(String email) throws Exception;
-  public boolean exist(String email, String password) throws Exception;
   public void insert(Member member) throws Exception;
   public void update(Member member) throws Exception;
   public void delete(int memberNo) throws Exception;
   public Member getOne(String email) throws Exception;
+  public Member getOne(String email, String password) throws Exception;
 }
