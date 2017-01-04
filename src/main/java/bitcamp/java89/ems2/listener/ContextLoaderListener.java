f4bea5e4f08eb0868471c8504b89f54150797ff5
Jongwoo.dev <Jongwoo.dev@gmail.com>
2017-01-04 16:26:35 +0900

    v1.6 - 프론트 컨트롤러 패턴 적용 / 01/04 오후4시26분

--- src/main/java/bitcamp/java89/ems2/listener/ContextLoaderListener.java
+++ src/main/java/bitcamp/java89/ems2/listener/ContextLoaderListener.java
@@ -13,13 +13,13 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 //@WebListener  <--- 이 예제에서는 애노테이션 대신 web.xml에 리스너를 등록하였다.
 public class ContextLoaderListener implements ServletContextListener {
   public static ApplicationContext applicationContext;
-  
+
   @Override
   public void contextInitialized(ServletContextEvent sce) {
     try {
       applicationContext = new FileSystemXmlApplicationContext(
           "file:" + sce.getServletContext().getRealPath("/WEB-INF/conf/application-context.xml"));
-      
+
       System.out.println("ContextLoaderListener.contextInitialized() 실행 완료!");
       
     } catch (Exception e) {
@@ -29,6 +29,6 @@ public class ContextLoaderListener implements ServletContextListener {
 
   @Override
   public void contextDestroyed(ServletContextEvent sce) {
-    
+
   }
 }
